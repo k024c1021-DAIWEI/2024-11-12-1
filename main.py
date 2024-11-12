4f9c53755ebce4be22918fee205fb89a75389512
k024c1021-DAIWEI <sugedior19280303@gmail.com>
2024-11-12 06:54:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    username='リョウスケ'
+    age=22
+    email='keiji@example.com'
+
+    
+    # テンプレートエンジンにデータを指定
+    return render_template(
+            'card-age.html',
+            username=username,
+            age=age,
+            email=email)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
+
